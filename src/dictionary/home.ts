bf4768e0b93b52a8ff4cb8e42d75136a1a70913b
Brandon Palomino <12bpalomino@gmail.com>
2023-11-05 22:48:09 -0800

    home page basic

---
+++ src/dictionary/home.ts
@@ -0,0 +1,20 @@
+const dictionary = {
+  'page-heading': 'Elaine & Brandon',
+  brandon: 'Brandon',
+  elaine: 'Elaine',
+  and: 'and',
+  'wedding-date': 'April 27, 2024',
+  'wedding-city': 'Los Angeles, CA',
+  'how-we-met-heading': 'HOW WE MET',
+  'how-we-met-description':
+    'We met dancing bachata on the Fourth of July. Both of us wanted to be friends but after spending some time together carpooling to dance events, we both found ourselves more aware of one another.  We jumped in with both feet and found ourselves complementing each other and growing together. Through thick and thin, we choose to continue to support and love one another.',
+  'the-proposal-heading': 'THE PROPOSAL',
+  'the-proposal-date': '3.27.22',
+  'the-proposal-description': [
+    'At the end of an unusual rainy season, we went to Samuel P. Taylor SP to take a break and enjoy the green mountains. We got to the peak of our hike, took a break to enjoy the view, and started to pack up to go down when Brandon asked me to take a picture near the edge. I walked to where he pointed and when I turned around to face him, I saw him starting to kneel down with a ring in his hand.  I sobbed for quite some time but finally said "YES" and we were engaged.',
+    "We're so excited to celebrate this next chapter in our lives with you.",
+    'See you in Los Angeles!',
+  ],
+}
+
+export default dictionary
