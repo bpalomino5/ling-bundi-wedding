15bb4692001be5593b407e80e625fb97fe97eb97
Brandon Palomino <12bpalomino@gmail.com>
2023-11-06 00:43:32 -0800

    language-button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,18 @@ import dictionary from '@/dictionary/home'
 export default function Home() {
   return (
     <div>
+        <div className='dropdown dropdown-hover not-prose fixed top-5 right-5 z-[1]'>
+          <label tabIndex={0} className='btn m-1'>
+          <svg className="w-6 h-6" viewBox='0 0 24 24'>
+            <path d='M11.99 2C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zm6.93 6h-2.95c-.32-1.25-.78-2.45-1.38-3.56 1.84.63 3.37 1.91 4.33 3.56zM12 4.04c.83 1.2 1.48 2.53 1.91 3.96h-3.82c.43-1.43 1.08-2.76 1.91-3.96zM4.26 14C4.1 13.36 4 12.69 4 12s.1-1.36.26-2h3.38c-.08.66-.14 1.32-.14 2 0 .68.06 1.34.14 2H4.26zm.82 2h2.95c.32 1.25.78 2.45 1.38 3.56-1.84-.63-3.37-1.9-4.33-3.56zm2.95-8H5.08c.96-1.66 2.49-2.93 4.33-3.56C8.81 5.55 8.35 6.75 8.03 8zM12 19.96c-.83-1.2-1.48-2.53-1.91-3.96h3.82c-.43 1.43-1.08 2.76-1.91 3.96zM14.34 14H9.66c-.09-.66-.16-1.32-.16-2 0-.68.07-1.35.16-2h4.68c.09.65.16 1.32.16 2 0 .68-.07 1.34-.16 2zm.25 5.56c.6-1.11 1.06-2.31 1.38-3.56h2.95c-.96 1.65-2.49 2.93-4.33 3.56zM16.36 14c.08-.66.14-1.32.14-2 0-.68-.06-1.34-.14-2h3.38c.16.64.26 1.31.26 2s-.1 1.36-.26 2h-3.38z'></path>
+          </svg>
+          </label>
+          <ul tabIndex={0} className='p-2 shadow dropdown-content z-[1] menu bg-base-100 rounded-box w-auto'>
+            <li><a className="tooltip tooltip-left" data-tip="English">🇺🇸</a></li>
+            <li><a className="tooltip tooltip-left" data-tip="Español">🇵🇪</a></li>
+          </ul>
+        </div>
+
       <header className='prose max-w-none text-center mb-6 p-10'>
         <h1>{headerDictionary['page-heading']}</h1>
 
@@ -27,9 +39,9 @@ export default function Home() {
       </div>
 
       <main className='prose max-w-none text-center px-60'>
-        <div className="flex flex-row items-center justify-center relative py-20">
-          <div  className="bg-[url('https://images.zola.com/81efdc62-6bba-4b20-a923-c443fb467708?w=500')] bg-contain bg-no-repeat bg-center h-[415px] w-[178px] block shrink" />
-          <div className="flex-1 flex flex-col justify-center">
+        <div className='flex flex-row items-center justify-center relative py-20'>
+          <div className="bg-[url('https://images.zola.com/81efdc62-6bba-4b20-a923-c443fb467708?w=500')] bg-contain bg-no-repeat bg-center h-[415px] w-[178px] block shrink" />
+          <div className='flex-1 flex flex-col justify-center'>
             <h2 className='m-0 uppercase text-6xl'>{dictionary['brandon']}</h2>
             <p className='my-10'>{dictionary['and']}</p>
             <h2 className='m-0 uppercase text-6xl'>{dictionary['elaine']}</h2>
@@ -42,14 +54,14 @@ export default function Home() {
           <h4>{dictionary['wedding-city']}</h4>
         </div>
 
-        <div className="my-20">
-          <div className='bg-[url("https://images.zola.com/86fe04fe-0fc1-4f2c-b8c8-eb5bdf9f3db0?w=500")] bg-contain bg-no-repeat bg-center h-[89px] w-[180px] relative align-middle mx-auto'/>
+        <div className='my-20'>
+          <div className='bg-[url("https://images.zola.com/86fe04fe-0fc1-4f2c-b8c8-eb5bdf9f3db0?w=500")] bg-contain bg-no-repeat bg-center h-[89px] w-[180px] relative align-middle mx-auto' />
           <h3>{dictionary['how-we-met-heading']}</h3>
           <p>{dictionary['how-we-met-description']}</p>
         </div>
 
-        <div className="my-20">
-          <div className='bg-[url("https://images.zola.com/86fe04fe-0fc1-4f2c-b8c8-eb5bdf9f3db0?w=500")] bg-contain bg-no-repeat bg-center h-[89px] w-[180px] relative align-middle mx-auto'/>
+        <div className='my-20'>
+          <div className='bg-[url("https://images.zola.com/86fe04fe-0fc1-4f2c-b8c8-eb5bdf9f3db0?w=500")] bg-contain bg-no-repeat bg-center h-[89px] w-[180px] relative align-middle mx-auto' />
           <h3>{dictionary['the-proposal-heading']}</h3>
           <p>{dictionary['the-proposal-date']}</p>
           {dictionary['the-proposal-description'].map((paragraph, index) => (
